c0636c9b80af199c1e9aa6708a29bb826f5d782a
SwiderskJ <janswiderski@op.pl>
2023-02-09 15:36:29 +0100

    Create model Maze and migration

--- gameplay_app/models.py
+++ gameplay_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from hero_app.models import Hero
+
+EVENTS = (
+    (1, "One-way tunnel"),
+    (2, "Cross"),
+    (3, "End of tunel"),
+    (4, "Boss"),
+)
+
+
+class Maze(models.Model):
+    position = models.IntegerField()
+    events = models.IntegerField(choices=EVENTS, null=True, default=1)
+    difficulty = models.IntegerField(default=0)
+    hero = models.ManyToManyField(Hero, through='MazeHero')
+
+
+class MazeHero(models.Model):
+    hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
+    position = models.ForeignKey(Maze, on_delete=models.CASCADE)
+    last_position = models.IntegerField(default=1)
+    active_position = models.BooleanField
