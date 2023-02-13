49689e4669a6262b154e7f4c1b6492eb869218ce
SwiderskJ <janswiderski@op.pl>
2023-02-13 10:24:28 +0100

    rebuild monster model

--- monster_app/forms.py
+++ monster_app/forms.py
@@ -1,16 +1,21 @@
 from django import forms
-from monster_app.models import DICE
+from monster_app.models import EXPERIENCE
 
 
 class MonsterForm(forms.Form):
     name = forms.CharField()
-    level = forms.IntegerField()
     strength = forms.IntegerField()
     dexterity = forms.IntegerField()
-    wisdom = forms.IntegerField()
     endurance = forms.IntegerField()
-    difficult = forms.IntegerField()
+    intelligence = forms.IntegerField()
+    wisdom = forms.IntegerField()
+    charisma = forms.IntegerField()
+    max_health_points = forms.IntegerField()
     damage_reduction = forms.IntegerField()
-    number_of_dices = forms.IntegerField()
-    dice = forms.ChoiceField(choices=DICE)
+    number_of_attacks = forms.IntegerField()
+    damage = forms.IntegerField()
+    initiative = forms.IntegerField()
+    defence_bonus = forms.IntegerField()
+    difficult = forms.IntegerField()
+    experience = forms.ChoiceField(choices=EXPERIENCE)
 
--- monster_app/views.py
+++ monster_app/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.urls import reverse_lazy, reverse
 from monster_app.forms import MonsterForm
-from monster_app.models import Monster, DICE
-from django.template.defaultfilters import slugify
-from random import randint
+from monster_app.models import Monster
 
 
 class MonsterList(LoginRequiredMixin, View):  # Used to display a list of all the monsters stored in the database,
@@ -30,84 +28,78 @@ class CreateMonsterView(LoginRequiredMixin, View):  # This view is used to creat
         if form.is_valid():
             data = form.cleaned_data
 
-            monster = Monster.objects.create(
+            Monster.objects.create(
                 name=data.get('name'),
-                level=data.get('level'),
                 strength=data.get('strength'),
                 dexterity=data.get('dexterity'),
-                wisdom=data.get('wisdom'),
                 endurance=data.get('endurance'),
-                difficult=data.get('difficult'),
+                intelligence=data.get('intelligence'),
+                wisdom=data.get('wisdom'),
+                charisma=data.get('charisma'),
+                max_health_points=data.get('max_health_points'),
                 damage_reduction=data.get('damage_reduction'),
-                number_of_dices=data.get('number_of_dices'),
-                dice=data.get('dice'),
+                number_of_attacks=data.get('number_of_attacks'),
+                damage=data.get('damage'),
+                initiative=data.get('initiative'),
+                defence_bonus=data.get('defence_bonus'),
+                difficult=data.get('difficult'),
+                experience=data.get('experience'),
+
             )
-            # The monster's attributes are calculated based on the input from the form and stored in the database.
-            monster.max_health_points = monster.endurance * 4
-            monster.health_points = monster.max_health_points
-            monster.damage_bonus = monster.strength
-            monster.attack_bonus = monster.dexterity
-            monster.defence_bonus = monster.wisdom
-            monster.initiative = monster.wisdom + monster.dexterity
-            monster_dice = int(monster.dice)
-            monster_dice = DICE[monster_dice - 1]
-            monster_dice = monster_dice[1]
-            monster.damage = monster.number_of_dices * monster_dice
-            if monster.slug == 0:
-                monster.slug = slugify(monster.name + str(randint(1, 1000)) + str(monster.strength)
-                                       + str(monster.dexterity) + str(randint(1, 1000)))
-            monster.save()
 
             return redirect(reverse('create_monster'))
 
 
-class EditMonsterView(LoginRequiredMixin, View):  # This view is used to edit an existing monster in the database.
-    login_url = reverse_lazy('login')
-
-    def get(self, request, slug):
-        monster = Monster.objects.get(slug=slug)
-        form = MonsterForm(initial={
-            'name': monster.name,
-            'level': monster.level,
-            'strength': monster.strength,
-            'dexterity': monster.dexterity,
-            'wisdom': monster.wisdom,
-            'endurance': monster.endurance,
-            'difficult': monster.difficult,
-            'damage_reduction': monster.damage_reduction,
-            'number_of_dices': monster.number_of_dices,
-            'dice': monster.dice
-            })
-
-        return render(request, 'edit_monster.html', {'form': form, 'monster': monster})
-
-    def post(self, request, monster_id):
-        form = MonsterForm(request.POST)
-
-        if form.is_valid():
-            data = form.cleaned_data
-            monster = Monster.objects.get(id=monster_id)
-
-            monster.name = data.get('name')
-            monster.level = data.get('level')
-            monster.strength = data.get('strength')
-            monster.dexterity = data.get('dexterity')
-            monster.wisdom = data.get('wisdom')
-            monster.endurance = data.get('endurance')
-            monster.max_health_points = monster.endurance * 4
-            monster.health_points = monster.max_health_points
-            monster.difficult = data.get('difficult')
-            monster.damage_reduction = data.get('damage_reduction')
-            monster.number_of_dices = data.get('number_of_dices')
-            monster.dice = data.get('dice')
-            monster.damage_bonus = monster.strength
-            monster.attack_bonus = monster.dexterity
-            monster.defence_bonus = monster.wisdom
-            monster.initiative = monster.wisdom + monster.dexterity
-            monster_dice = int(monster.dice)
-            monster_dice = DICE[monster_dice - 1]
-            monster_dice = monster_dice[1]
-            monster.damage = monster_dice * monster.number_of_dices
-            monster.save()
-
-            return redirect(reverse('monster_list'))
+# class EditMonsterView(LoginRequiredMixin, View):  # This view is used to edit an existing monster in the database.
+#     login_url = reverse_lazy('login')
+#
+#     def get(self, request, slug):
+#         monster = Monster.objects.get(slug=slug)
+#         form = MonsterForm(initial={
+#             'name': monster.name,
+#             'level': monster.level,
+#             'strength': monster.strength,
+#             'dexterity': monster.dexterity,
+#             'endurance': monster.wisdom,
+#             'intelligence': monster.endurance,
+#             'wisdom': monster.difficult,
+#             'charisma': monster.damage_reduction,
+#             'difficult': monster.number_of_dices,
+#             'damage_reduction': monster.dice,
+#             'number_of_attacks': monster.number_of_attacks,
+#             'damage': monster.damage,
+#             'experience': monster.experience,
+#             })
+#
+#         return render(request, 'edit_monster.html', {'form': form, 'monster': monster})
+#
+#     def post(self, request, monster_id):
+#         form = MonsterForm(request.POST)
+#
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             monster = Monster.objects.get(id=monster_id)
+#
+#             monster.name = data.get('name')
+#             monster.level = data.get('level')
+#             monster.strength = data.get('strength')
+#             monster.dexterity = data.get('dexterity')
+#             monster.wisdom = data.get('wisdom')
+#             monster.endurance = data.get('endurance')
+#             monster.max_health_points = monster.endurance * 4
+#             monster.health_points = monster.max_health_points
+#             monster.difficult = data.get('difficult')
+#             monster.damage_reduction = data.get('damage_reduction')
+#             monster.number_of_dices = data.get('number_of_dices')
+#             monster.dice = data.get('dice')
+#             monster.damage_bonus = monster.strength
+#             monster.attack_bonus = monster.dexterity
+#             monster.defence_bonus = monster.wisdom
+#             monster.initiative = monster.wisdom + monster.dexterity
+#             monster_dice = int(monster.dice)
+#             monster_dice = DICE[monster_dice - 1]
+#             monster_dice = monster_dice[1]
+#             monster.damage = monster_dice * monster.number_of_dices
+#             monster.save()
+#
+#             return redirect(reverse('monster_list'))
