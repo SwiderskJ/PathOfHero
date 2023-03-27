92ebd369bbca01cb4ab6e5ef924d4e2f6655546d
SwiderskJ <janswiderski@op.pl>
2023-03-27 18:48:26 +0200

    test1

--- user_app/views.py
+++ user_app/views.py
@@ -84,7 +84,7 @@ class UserCreateView(View):  # This view is responsible for handling user regist
 
         if form.is_valid():
             data = form.cleaned_data
-            if User.objects.get(username=data.get('login')):
+            if User.objects.filter(username=data.get('login')):
                 return redirect(reverse('register'))
 
             user = User.objects.create_user(
